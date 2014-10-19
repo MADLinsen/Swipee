82d4c1d1aaa92f38b810628c4f8d8be776bc516f
MADLinsen <linsenwu@comcast.net>
2014-10-18 19:26:15 -0700

    Changed animation stuff

--- home.js
+++ home.js
@@ -7,24 +7,28 @@
 			console.log("Event Name:" + newPost.event_name);
 		});
     
-    $scope.invites = {};
-    $scope.invites[0] = {type: "event", name: "Seahawks game", time: "13:00", location: "Wilburt's House"};
-    $scope.invites[1] = {type: "event", name: "Football", time: "18:00", location: "IMA"};
-    $scope.invites[2] = {type: "event", name: "Study", time: "9:00", location: "Odegaard"};
-    $scope.invites[3] = {type: "event", name: "Dinner", time: "19:00", location: "Chipotle"};
+    
+    $scope.invites = [];
+    $scope.invites[0] = {type: "event", name: "Seahawks game", time: "13:00", location: "Wilburt's House", color: "blue"};
+    $scope.invites[1] = {type: "event", name: "Football", time: "18:00", location: "IMA", color: "green"};
+    $scope.invites[2] = {type: "event", name: "Study", time: "9:00", location: "Odegaard", color: "red"};
+    $scope.invites[3] = {type: "event", name: "Dinner", time: "19:00", location: "Chipotle", color: "purple"};
     
     $scope.clickInvite = function(event) {
-      $(".collapse").collapse({'toggle': false});
-      $(event.target).next(".collapse").collapse('toggle');
-      $(".collapse").not(event.target).collapse('hide');
+      if (!$(event.target).hasClass("arrows")) {
+        var info = $(event.target).next(".info");
+        if (info.css("height") == "0px") {
+          info.animate({height: "75px", opacity: 10}, 100);
+        }
+        else {
+          info.animate({height: "0px", opacity: 0}, 100);
+        }
+        $(".info").not(info).animate({height: "0px", opacity: 0}, 100);    
+      }
     }
-
-    $(".collapse").click(function(event) {
-      $(this).collapse('toggle');
-    });
     
     $scope.clickLeftArrow = function(event) {
-      $(event.target).parent(".inv").next(".collapse").animate({ marginRight: '90%' }, 500, function() {
+      $(event.target).parent(".inv").next(".info").animate({ marginRight: '90%' }, 500, function() {
         $(this).remove();
       });
       $(event.target).parent(".inv").animate({ marginRight: '90%' }, 500, function() {
@@ -33,7 +37,7 @@
     }
     
     $scope.clickRightArrow = function(event) {
-      $(event.target).parent(".inv").next(".collapse").animate({ marginLeft: '90%' }, 500, function() {
+      $(event.target).parent(".inv").next(".info").animate({ marginLeft: '90%' }, 500, function() {
         $(this).remove();
       });
       $(event.target).parent(".inv").animate({ marginLeft: '90%' }, 500, function() {
