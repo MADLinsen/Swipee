7086a1c518149cd1b698b7269862e801121760a9
MADLinsen <linsenwu@comcast.net>
2014-10-18 18:06:49 -0700

    Added angular shit

--- home.js
+++ home.js
@@ -7,43 +7,39 @@
 			console.log("Event Name:" + newPost.event_name);
 		});
     
-    $(".collapse").collapse({'toggle': false});
-    $(".inv").click(function(event) {
-      
-      // Is collapsed
-      if (!$(this).next(".collapse").is(":visible") && $(event.target).hasClass("inv")) {
-        $(this).next(".collapse").collapse('toggle');
-        $(".collapse").not(this).collapse('hide');
-      }
-      // Is expanded
-      if ($(this).next(".collapse").is(":visible")) {
-        $(this).next(".collapse").collapse('toggle');
-      }
-    });
+    $scope.invites = {};
+    $scope.invites[0] = {type: "event", name: "Seahawks game", time: "13:00", location: "Wilburt's House"};
+    $scope.invites[1] = {type: "event", name: "Football", time: "18:00", location: "IMA"};
+    $scope.invites[2] = {type: "event", name: "Study", time: "9:00", location: "Odegaard"};
+    $scope.invites[3] = {type: "event", name: "Dinner", time: "19:00", location: "Chipotle"};
     
+    $scope.clickInvite = function(event) {
+      $(".collapse").collapse({'toggle': false});
+      $(event.target).next(".collapse").collapse('toggle');
+      $(".collapse").not(event.target).collapse('hide');
+    }
+
     $(".collapse").click(function(event) {
       $(this).collapse('toggle');
     });
     
-    $(".arrow-left").click(function(event) {
-      var collapse = $(this).parent(".inv").next(".collapse");
-      collapse.animate({ marginLeft: '90%' }, 500, function() {
+    $scope.clickLeftArrow = function(event) {
+      $(event.target).parent(".inv").next(".collapse").animate({ marginRight: '90%' }, 500, function() {
         $(this).remove();
       });
-      $(this).parent(".inv").animate({ marginRight: '90%' }, 500, function() {
+      $(event.target).parent(".inv").animate({ marginRight: '90%' }, 500, function() {
         $(this).remove();
-        $(this).next(".collapse").remove();
       });
-    });
+    }
     
-    $(".arrow-right").click(function(event) {
-      $(this).parent(".inv").next(".collapse").animate({ marginLeft: '90%' }, 500, function() {
+    $scope.clickRightArrow = function(event) {
+      $(event.target).parent(".inv").next(".collapse").animate({ marginLeft: '90%' }, 500, function() {
         $(this).remove();
       });
-      $(this).parent(".inv").animate({ marginLeft: '90%' }, 500, function() {
+      $(event.target).parent(".inv").animate({ marginLeft: '90%' }, 500, function() {
         $(this).remove();
       });
-    });
+    }
     
 	}]);
 }());
